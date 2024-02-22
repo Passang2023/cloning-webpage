c6d69e7fad345e3d4e2149f545b5501f1e400690
Passang2023 <155144516+Passang2023@users.noreply.github.com>
2024-02-22 14:18:38 +0600

    Add files via upload

---
+++ src/data/constants.js
@@ -0,0 +1,220 @@
+export const Bio = {
+  name: "PASSANG LHAMO",
+  roles: [
+    "Full Stack Developer",
+    "Wordpress Developer",
+    "Graphic Designer",
+  
+  ],
+  description:
+    "I am a motivated and versatile individual, always eager to take on new challenges. With a passion for learning I am dedicated to delivering high-quality results. With a positive attitude and a growth mindset, I am ready to make a meaningful contribution and achieve great things.",
+  github: "https://saifurrahman95.com",
+  resume:
+    "https://drive.google.com/file/d/1WSp2hQ1WiPWrxC1OE2jqMu-1esbCzXgY/view?usp=sharing",
+  linkedin: "https://www.linkedin.com",
+  twitter: "https://twitter.com",
+  insta: "https://www.instagram.com/",
+  facebook: "https://www.facebook.com",
+};
+
+export const skills = [
+  {
+    title: "Frontend",
+    skills: [
+      {
+        name: "React Js",
+        image:
+          "data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHZpZXdCb3g9Ii0xMS41IC0xMC4yMzE3NCAyMyAyMC40NjM0OCI+CiAgPHRpdGxlPlJlYWN0IExvZ288L3RpdGxlPgogIDxjaXJjbGUgY3g9IjAiIGN5PSIwIiByPSIyLjA1IiBmaWxsPSIjNjFkYWZiIi8+CiAgPGcgc3Ryb2tlPSIjNjFkYWZiIiBzdHJva2Utd2lkdGg9IjEiIGZpbGw9Im5vbmUiPgogICAgPGVsbGlwc2Ugcng9IjExIiByeT0iNC4yIi8+CiAgICA8ZWxsaXBzZSByeD0iMTEiIHJ5PSI0LjIiIHRyYW5zZm9ybT0icm90YXRlKDYwKSIvPgogICAgPGVsbGlwc2Ugcng9IjExIiByeT0iNC4yIiB0cmFuc2Zvcm09InJvdGF0ZSgxMjApIi8+CiAgPC9nPgo8L3N2Zz4K",
+      },
+    
+      {
+        name: "HTML",
+        image: "https://www.w3.org/html/logo/badge/html5-badge-h-solo.png",
+      },
+      {
+        name: "CSS",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/d/d5/CSS3_logo_and_wordmark.svg/1452px-CSS3_logo_and_wordmark.svg.png",
+      },
+      {
+        name: "JavaScript",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/6/6a/JavaScript-logo.png/800px-JavaScript-logo.png",
+      },
+      {
+        name: "Bootstrap",
+        image:
+          "https://getbootstrap.com/docs/5.3/assets/brand/bootstrap-logo-shadow.png",
+      },
+     
+    ],
+  },
+  {
+    title: "Backend",
+    skills: [
+      {
+        name: "Node Js",
+        image: "https://nodejs.org/static/images/logo.svg",
+      },
+      {
+        name: "MongoDB",
+        image:
+          "https://raw.githubusercontent.com/devicons/devicon/master/icons/mongodb/mongodb-original-wordmark.svg",
+      },
+    ],
+  },
+ 
+
+  {
+    title: "Others",
+    skills: [
+      {
+        name: "Git",
+        image:
+          "https://e7.pngegg.com/pngimages/713/558/png-clipart-computer-icons-pro-git-github-logo-text-logo-thumbnail.png",
+      },
+      {
+        name: "GitHub",
+        image:
+          "https://github.githubassets.com/images/modules/logos_page/GitHub-Mark.png",
+      },
+      {
+        name: "VS Code",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/9/9a/Visual_Studio_Code_1.35_icon.svg/512px-Visual_Studio_Code_1.35_icon.svg.png?20210804221519",
+      },
+      {
+        name: "Postman",
+        image:
+          "https://static-00.iconduck.com/assets.00/postman-icon-497x512-beb7sy75.png",
+      },
+      {
+        name: "Figma",
+        image:
+          "https://s3-alpha.figma.com/hub/file/1481185752/fa4cd070-6a79-4e1b-b079-8b9b76408595-cover.png",
+      },
+    ],
+  },
+];
+
+export const experiences = [
+  {
+   
+    id: 0,
+    img: "https://firebasestorage.googleapis.com/v0/b/flexi-coding.appspot.com/o/flipr.jpeg?alt=media&token=1d72532a-45eb-4c1a-a81a-c9bed9fec543",
+    role: "Fullstack Learner",
+    company: "Lancercode",
+    date: "Jan 2023 - Apirl 2023",
+    desc: "I recently completed the web development course at LancerCode and I must say it was an exceptional experience. The instructors were knowledgeable and provided clear, easy-to-follow lessons that made learning web development enjoyable and rewarding. The course content was comprehensive, and I felt well-prepared to take on real-world projects by the time I completed the program. I highly recommend LancerCode to anyone looking to learn web development. ",
+    skills: [
+      "ReactJS",
+      "NodeJs",
+      "HTML",
+      "CSS",
+      "JavaScript",
+      "MongoDB",
+    ],
+    
+  },
+  {
+    id: 1,
+    img: "https://firebasestorage.googleapis.com/v0/b/flexi-coding.appspot.com/o/gdsc.jpeg?alt=media&token=c162329c-efaa-4be8-a173-8d3f4c48ea70",
+    role: "Wordpress Learner",
+    company: "Lancercode",
+    date: "Jan 2024 - Apr 2024",
+    desc: "Studying WordPress at LancerCode has been an enlightening and rewarding journey. The instructors are knowledgeable and provide hands-on experience that has significantly boosted my understanding of the platform. The course material is well-structured and easy to follow, making the learning process enjoyable. I feel more confident in my abilities to create and manage WordPress websites after completing the program.",
+    skills: [
+      "Figma",
+    ],
+  },
+  {
+    id: 2,
+    img: "https://firebasestorage.googleapis.com/v0/b/flexi-coding.appspot.com/o/girlScript.jpeg?alt=media&token=e656a621-cf3c-4230-bf0f-e74b4cec6035",
+    role: "Web Desgin ",
+    company: "Lancercode",
+    date: "Feb 2024 - Present",
+    desc: "The web design course at LancerCode is fantastic! I came into this course with little to no knowledge of web design, and now I feel confident in creating my own websites.",
+    skills: [
+      "Java",
+      "JavaScript",
+      "TypeScript",
+      "Node Js",
+      "HTML",
+      "CSS",
+    ],
+  },
+];
+
+export const education = [
+  {
+    id: 0,
+    school: "Ugyen Academy",
+    date: "Feb 2017 - June 2019",
+    grade: "11/12",
+    desc: "I completed my higher  education at Ugyen Academy .",
+    degree: "I studied ARTS",
+  },
+
+  {
+    id: 1,
+    school: "Gyelposhing DSP Center, Mongar",
+    date: "Jan 2024 - April 2024",
+    grade: "3",
+    desc: "ICT Foundation courses or programs are designed to provide individuals with the necessary skills and knowledge to effectively use and navigate various technologies in both personal and professional settings. This may include learning how to use common software applications like Microsoft Office, understanding basic computer hardware and software components, developing digital literacy skills, and exploring online communication and collaboration tools.",
+    degree: "ICT Advancr",
+  },
+ 
+];
+
+export const projects = [
+  {
+    id: 11,
+    title: "Website",
+    date: "Jan 2024 - Dec 2023",
+    description:
+      "Building a website can sometimes be frustrating, especially if you encounter technical challenges or if things don't turn out how you envisioned them. It may take multiple attempts to get things right, which can be frustrating.",
+    image:
+    "https://firebasestorage.googleapis.com/v0/b/flexi-coding.appspot.com/o/girlScript.jpeg?alt=media&token=e656a621-cf3c-4230-bf0f-e74b4cec6035",
+    tags: [
+      "React Js",
+      "Node Js",
+      "Express Js",
+    ],
+    category: "web app",
+   
+  },
+ 
+  {
+    id: 0,
+    title: "Boostrap",
+    date: " Jan 2024 - Apr 2024",
+    description:
+      "Bootstrap is a popular front-end framework that helps developers build responsive and mobile-first websites. It provides a wide range of pre-designed components and utilities that can be easily integrated into a project to create a visually appealing and user-friendly interface.",
+    image:
+      "https://getbootstrap.com/docs/5.3/assets/brand/bootstrap-logo-shadow.png",
+    tags: ["Boostrap"],
+    category: "web app",
+    
+  },
+  {
+    id: 4,
+    title: "portfolio Clone",
+    date: "Feb 2024",
+    description:
+      "A website clone is a copy or reproduction of a particular website that mimics the design, functionality, and features of the original site. Website clones are often created for purposes such as research, educational use, testing, or as a template for creating a similar website. Cloning a website can help developers understand how a particular website is built and functions, but it is important to ensure that copyright laws and ethical considerations are respected when creating a website clone.",
+    image:
+    "https://www.w3.org/html/logo/badge/html5-badge-h-solo.png",
+    tags: ["React Js", "Node JS"],
+    category: "web app",
+  },
+  
+ 
+];
+
+export const TimeLineData = [
+  { year: 2017, text: "Started my journey" },
+  { year: 2018, text: "Worked as a freelance developer" },
+  { year: 2019, text: "Founded JavaScript Mastery" },
+  { year: 2020, text: "Shared my projects with the world" },
+  { year: 2021, text: "Started my own platform" },
+];
